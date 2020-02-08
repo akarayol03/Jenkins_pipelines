117a504e28a3dea23ab105ab03763687e6873794
akarayol03 <akarayol03@gmail.com>
2020-02-08 12:01:36 -0600

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
 node {
-    properties([parameters([choice(choices: 
-    ['golden_ami', 'tower', 'elk', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'), 
-    choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose  a region', name: 'AMI_REGION')])])
+    properties([parameters([
+        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'),
+        text(defaultValue: 'dummy1@gmail.com', description: 'Please provide email(s) for notifications. Use , for multiple emails', name: 'EMAIL_TO_SEND'),
+        choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose a region', name: 'AMI_REGION')])])
 
     stage ("Pull Repo"){
         git 'https://github.com/akarayol03/packer-1.git'

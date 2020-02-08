ee567cabaa26a2a98e45e7c0fddb2f92f054d905
akarayol03 <akarayol03@gmail.com>
2020-02-08 11:12:20 -0600

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,16 @@
 node {
-
-    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
-    stage("pull Repo"){
+    properties([parameters([choice(choices: ['us-east-1', 'us-west-1', 'us-west-2'], description: 'please select a region', name: 'ami_region')])])
+    stage ("Pull Repo"){
         git 'https://github.com/akarayol03/packer-1.git'
-
     }
-    stage("Build Image"){
+    stage ("Build Image"){
         sh "packer version"
-        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"added
-        
+        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
     }
-    stage("send Notification to Slack"){
+    stage ("Send Notification to Slack"){
         echo "Hello"
-        
     }
-    stage("Send Email"){
-        echo "Hello"
-        
+    stage ("Send Email"){
+        echo "Hellp"
     }
 }

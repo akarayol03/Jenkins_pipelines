5218486bb6e7d00c3ab85e22fb322b0ac69322f0
akarayol03 <akarayol03@gmail.com>
2020-02-08 10:32:34 -0600

    pipeline added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,17 @@
+node {
+    stage("pull Repo"){
 
+    }
+    stage("Build Image"){
+        sh "packer version"
+        
+    }
+    stage("send Notification to Slack"){
+        echo "Hello"
+        
+    }
+    stage("Send Email"){
+        echo "Hello"
+        
+    }
+}

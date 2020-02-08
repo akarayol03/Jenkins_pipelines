7528a124a953e0131c1a4e85685247a7d45f95ce
akarayol03 <akarayol03@gmail.com>
2020-02-08 11:02:23 -0600

    region option added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,10 +2,12 @@ node {
 
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
     stage("pull Repo"){
+        git 'https://github.com/akarayol03/packer-1.git'
 
     }
     stage("Build Image"){
         sh "packer version"
+        sh "packer bild -var region=${AMI_REGION} tools/jenkins_example.json
         
     }
     stage("send Notification to Slack"){

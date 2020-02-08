e465af2d4bea58c07904bffdba05d6fa7b57150e
akarayol03 <akarayol03@gmail.com>
2020-02-08 11:29:20 -0600

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,10 @@
 node {
-    properties([parameters([choice(choices: ['us-east-1', 'us-west-1', 'us-west-2'], description: 'please select a region', name: 'ami_region')])])
+    properties(
+        [parameters([choice(choices: 
+        ['us-east-1', 'us-west-1', 'us-west-2'], 
+        description: 'please select a region', 
+        name: 'ami_region')])])
+        
     stage ("Pull Repo"){
         git 'https://github.com/akarayol03/packer-1.git'
     }
